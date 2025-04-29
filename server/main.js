937b26a7f0f3094c99833f86b6c1f9de2be4f388
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-04-29 15:28:46 -0500

    perf: :zap: Added Main js file.

---
+++ server/main.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const dbClient = require("./mongoDb.js");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Servidor corriendo y conectado a MongoDB");
+});
+
+app.listen(3000, () => console.log("Servidor escuchando en el puerto 3000"));
