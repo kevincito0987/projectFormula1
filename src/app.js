d3767126bae7a0c2c334f66e771f23b119c8337a
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-05-01 18:38:49 -0500

    perf: :zap: I get data of news api.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
 document.querySelector("news-links").addEventListener("news-selected", (event) => {
     console.log("Seleccionaste:", event.detail.newsType); // 🔥 Muestra la categoría seleccionada
 });
+
+async function getLatestF1News() {
+    const url = "https://newsdata.io/api/1/news?apikey=pub_8360532528164487c638046bd83af7a3690b1&q=formula%201";
+    
+    try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error(`Error en la solicitud: ${response.status}`);
+        
+        const data = await response.json();
+        const latestNews = data.results.slice(0, 10); // 📌 Extraer solo las primeras 10 noticias
+        
+        console.log("Últimas noticias de F1:", latestNews); // 🔥 Muestra las noticias en la consola
+        return latestNews;
+    } catch (error) {
+        console.error("Error obteniendo noticias:", error);
+        return [];
+    }
+}
+
+getLatestF1News()
