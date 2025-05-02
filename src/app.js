d29d18f62f4ad1372a13d39d005dad4cdf1e46f1
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-05-02 18:19:03 -0500

    feat: :sparkles: Limpiando repositorio

--- src/app.js
+++
@@ -1,3 +0,0 @@
-document.querySelector("news-links").addEventListener("news-selected", (event) => {
-    console.log("Seleccionaste:", event.detail.newsType); // 🔥 Muestra la categoría seleccionada
-});
