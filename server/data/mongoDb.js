9587a70a2addac4468823542e6e27c4f1bcb5f1c
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-05-03 14:19:31 -0500

    perf: :zap: I've been deploy it locally.

--- server/data/mongoDb.js
+++ server/data/mongoDb.js
@@ -1,17 +1,15 @@
-const path = require("path");
-require("dotenv").config({ path: path.join(__dirname, "../../.env") });
+require("dotenv").config({ path: "./server/.env" }); // 📌 Ajusta la ruta si es necesario
+
 const mongoose = require("mongoose");
 
 async function connectDB() {
     try {
-        console.log("🔍 Conectando a:", process.env.MONGO_URI); // 🔎 Verifica que la variable se está cargando
-
+        console.log("🔗 Conectando a:", process.env.MONGO_URI); // ✅ Verifica que la variable se está cargando
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-
-        console.log("✅ ¡Conexión exitosa a MongoDB Atlas! 🏁");
+        console.log("✅ ¡Conexión exitosa a MongoDB!");
     } catch (error) {
         console.error("❌ Error al conectar con MongoDB:", error.message);
         process.exit(1);
