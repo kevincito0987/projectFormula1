073c840df8161250d1498104ed3e99fc8e6a5e3c
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-05-03 15:08:52 -0500

    perf: :zap: I'm doingsome changes in general theme finished.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
 document.querySelector("news-links").addEventListener("news-selected", (event) => {
     console.log("Seleccionaste:", event.detail.newsType); // 🔥 Muestra la categoría seleccionada
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const video = document.querySelector("video");
+    
+    video.volume = 0.5; // 🔊 Ajusta el volumen al 50%
+});
