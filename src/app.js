82d91d6562d1dd605c9c24873d1501bc10f0c34b
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-05-04 09:30:11 -0500

    per: :zap: I've added user config in app.js.

--- src/app.js
+++ src/app.js
@@ -2,3 +2,31 @@ document.querySelector("news-links").addEventListener("news-selected", (event) =
     console.log("Seleccionaste:", event.detail.newsType); // 🔥 Muestra la categoría seleccionada
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+    const profileIcon = document.getElementById("profile-icon");
+    const homeIcon = document.getElementById("home-icon");
+    const mainContainer = document.querySelector("main");
+    const originalContent = mainContainer.innerHTML;
+
+    profileIcon.addEventListener("click", (event) => {
+        event.preventDefault();
+        mainContainer.innerHTML = "<user-config></user-config>";
+        
+        const userConfig = document.querySelector("user-config");
+        userConfig.addEventListener("nameChange", (event) => {
+            console.log("Nuevo nombre cambiado a:", event.detail);
+        });
+
+        userConfig.addEventListener("logout", () => {
+            document.body.classList.add("fade-slide");
+            setTimeout(() => {
+                window.location.href = "login.html";
+            }, 700);
+        });
+    });
+
+    homeIcon.addEventListener("click", (event) => {
+        event.preventDefault();
+        mainContainer.innerHTML = originalContent;
+    });
+});
